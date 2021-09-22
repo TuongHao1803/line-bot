b5f0c70c8a9745e5a3c552c09dad2bcffc2deb8e
Andy Shih <Tuonghao0318@icloud.com>
2021-09-22 20:28:55 +1000

    new

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃飯了嗎?'
+    s = '你吃飯了嗎？'
+    a = '你在幹嘛？'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
